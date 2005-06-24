42cab7dcd6b21258ce78a375bfb908ea481c78f1
rcameron <rcameron@eb686afc-39fa-0310-a79f-9b3c69cefa1c>
2005-06-24 22:51:54 +0000

    Minor extra work to sort out Diwaker's patch. Made it pass existing tests. Added an extra test case for his changes, and made journal title regexp a bit more flexible.
    git-svn-id: http://svn.citeulike.org/svn/plugins@9 eb686afc-39fa-0310-a79f-9b3c69cefa1c

--- python/usenix.py
+++ python/usenix.py
@@ -18,12 +18,13 @@ content = f.read()
 title_m  = re.search("<H2>(.*)\s*</H2>", content, re.IGNORECASE)
 if title_m != None:
     title = title_m.group(1)
-    print "title\t%s" % title
+    print "title\t%s" % title.strip()
+else:
+    print "title\tuntitled"
 
 author_m = re.search("</H2>\s*(.*),\s*<i>", content, re.IGNORECASE)
 if author_m != None:
     tmp = author_m.group(1).split('and')
-    print tmp
     if len(tmp) > 1:
         authors = tmp[0].split(", ")
         if authors[len(authors)-1]=='':
@@ -32,14 +33,14 @@ if author_m != None:
         else:
             authors = tmp
     for author in authors:
-        print "author\t%s" % author
+        print "author\t%s" % author.strip()
 
 pages_m = re.search("(\d+)&#150;(\d+)", content, re.IGNORECASE)
 if pages_m != None:
     start_page = pages_m.group(1)
     end_page = pages_m.group(2)
-    print "start_page\t%s" % start_page
-    print "end_page\t%s" % end_page
+    print "start_page\t%s" % start_page.strip()
+    print "end_page\t%s" % end_page.strip()
 
 abstract_m = re.search("</H3>(.*)<UL>", content, re.DOTALL | re.IGNORECASE)
 abstract = abstract_m.group(1)
@@ -47,7 +48,12 @@ abstract = abstract_m.group(1)
 journal_m = re.search("<b>(.*)\s*&#151;\s*Abstract(</b>)?</font>", content, re.DOTALL | re.IGNORECASE)
 if journal_m != None:
     journal = journal_m.group(1)
-    print "journal\t%s" % journal
+    print "journal\t%s" % journal.strip()
+else:
+    journal_m = re.search("<HEAD><TITLE>(.{,20} \'\d+)", content, re.DOTALL | re.IGNORECASE)
+    if journal_m != None:
+        journal = journal_m.group(1)
+        print "journal\t%s" % journal.strip()
 
 
 #print "abstract %s" % abstract
