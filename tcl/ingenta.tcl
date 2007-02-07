5fc92be21209df7657602a131e19c7f315a73e0a
rcameron <rcameron@eb686afc-39fa-0310-a79f-9b3c69cefa1c>
2007-02-07 14:17:41 +0000

    More informating error message on Ingenta when you try to bookmark an entire journal
    git-svn-id: http://svn.citeulike.org/svn/plugins@82 eb686afc-39fa-0310-a79f-9b3c69cefa1c

--- tcl/ingenta.tcl
+++ tcl/ingenta.tcl
@@ -40,6 +40,11 @@
 source util.tcl
 
 set url [gets stdin]
+
+if {[regexp {ingentaconnect.com/content/[^/]+/[^/]+/[^/]+/[^/]+/[^/]+$} $url]} {
+    bail "It appears you are trying to bookmark an entire issue of a journal. Try navigating to the abstract page for just one article on Ingenta and bookmark that instead (if you're trying to choose which article you want by using the check-boxes on the page then, regrettably, that won't work)."
+}
+
 set page [url_get $url]
 
 proc get_meta {page name} {
