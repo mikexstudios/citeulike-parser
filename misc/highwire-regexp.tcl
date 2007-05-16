e737aec5df4ae3e3f029530ed1cfe67d97be50b7
rcameron <rcameron@eb686afc-39fa-0310-a79f-9b3c69cefa1c>
2007-05-16 20:36:35 +0000

    Change highwire plugin to support stke.sciencemag.org
    git-svn-id: http://svn.citeulike.org/svn/plugins@88 eb686afc-39fa-0310-a79f-9b3c69cefa1c

--- misc/highwire-regexp.tcl
+++ misc/highwire-regexp.tcl
@@ -1,4 +1,4 @@
 set f [open "highwire-journal-list.txt" "r"]
 set body [join [split [read $f] "\n"] "|"]
 set body [regsub -all {\.} $body {\\.}]
-puts "http://($body)/cgi(/|/content/)(abstract|short|extract|full|refs|reprint|screenpdf|summary)\[A-Za-z0-9.-/\]+"
+puts "http://($body)/cgi(/|/content/)(abstract|short|extract|full|refs|reprint|screenpdf|summary)\[A-Za-z0-9.-/;\]+"
--- perl/highwire.pl
+++ perl/highwire.pl
@@ -67,7 +67,7 @@ $url = <>;
 #  Published articles, determine journal,volume,number and page details. 
 #  Create URL that links to abstract.
 
-if ($url =~ m{http://(.*)/cgi(/|/content/)(abstract|short|extract|full|refs|reprint|screenpdf|summary)/([0-9]+)/([0-9]+)/([0-9]+|[i|v|x|l|c|d|m]+|[I|V|X|L|C|D|M]+)})	 
+if ($url =~ m{http://(.*)/cgi(/|/content/)(abstract|short|extract|full|refs|reprint|screenpdf|summary)/(?:[a-zA-Z]+;)?([0-9]+)/([0-9]+)/((?:pe)?[0-9]+|[i|v|x|l|c|d|m]+|[I|V|X|L|C|D|M]+)})
 	{
 	($journal_site,$volume,$number,$page) = ($1,$4,$5,$6);
 	$url_abstract = "http://$journal_site/cgi/content/abstract/$volume/$number/$page";
@@ -94,7 +94,6 @@ else
 	print "status\terr\t (1) This does not appear to be a Highwire Press article. Try posting the article from the abstract page.\n" and exit;
 	}
 	
-
 
 
 # Get the link to the citebuilder url and formulate a link to the reference manager RIS file
