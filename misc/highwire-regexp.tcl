95cae3167550415080742d3ee2e6031719727708
rcameron <rcameron@eb686afc-39fa-0310-a79f-9b3c69cefa1c>
2007-07-23 15:49:45 +0000

    Allow highwire article type of 'long'. Don't care whether we get a www. as a prefix for the URL.
    git-svn-id: http://svn.citeulike.org/svn/plugins@109 eb686afc-39fa-0310-a79f-9b3c69cefa1c

--- misc/highwire-regexp.tcl
+++ misc/highwire-regexp.tcl
@@ -1,4 +1,12 @@
 set f [open "highwire-journal-list.txt" "r"]
-set body [join [split [read $f] "\n"] "|"]
+set r_lines [split [read $f] "\n"]
+set n_lines [list]
+foreach line $r_lines {
+	if {[string range $line 0 3] eq "www."} {
+		set line [string range $line 4 end]
+	}
+	lappend n_lines $line
+}
+set body [join $n_lines "|"]
 set body [regsub -all {\.} $body {\\.}]
-puts "http://($body)/cgi(/|/content/)(abstract|short|extract|full|refs|reprint|screenpdf|summary|eletters)\[A-Za-z0-9.-/;\]+"
+puts "http://(www\\.)?($body)/cgi(/|/content/)(abstract|short|long|extract|full|refs|reprint|screenpdf|summary|eletters)\[A-Za-z0-9.-/;\]+"
--- perl/highwire.pl
+++ perl/highwire.pl
@@ -67,7 +67,7 @@ $url = <>;
 #  Published articles, determine journal,volume,number and page details. 
 #  Create URL that links to abstract.
 
-if ($url =~ m{http://(.*)/cgi(/|/content/)(abstract|short|extract|full|refs|reprint|screenpdf|summary|eletters)/((?:[a-zA-Z]+;)?[0-9]+)/([0-9]+)/([A-Za-z0-9.]+)})
+if ($url =~ m{http://(.*)/cgi(/|/content/)(abstract|short|long|extract|full|refs|reprint|screenpdf|summary|eletters)/((?:[a-zA-Z]+;)?[0-9]+)/([0-9]+)/([A-Za-z0-9.]+)})
 	{
 	($journal_site,$volume,$number,$page) = ($1,$4,$5,$6);
 	$url_abstract = "http://$journal_site/cgi/content/abstract/$volume/$number/$page";
