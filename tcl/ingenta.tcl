bc14377f1014e583734052d3fe3a2f60b91fbb12
chall <chall@eb686afc-39fa-0310-a79f-9b3c69cefa1c>
2008-04-02 14:33:19 +0000

    Tweak for new ingent layout. Add ISSN and Journal title scraping.
    git-svn-id: http://svn.citeulike.org/svn/plugins@175 eb686afc-39fa-0310-a79f-9b3c69cefa1c

--- tcl/ingenta.tcl
+++ tcl/ingenta.tcl
@@ -42,7 +42,7 @@ source util.tcl
 set url [gets stdin]
 
 if {[regexp {ingentaconnect.com[^/]*/content/[^/]+/[^/]+/[^/]+/[^/]+/[^/]+$} $url]} {
-    bail "It appears you are trying to bookmark an entire issue of a journal. Try navigating to the abstract page for just one article on Ingenta and bookmark that instead (if you're trying to choose which article you want by using the check-boxes on the page then, regrettably, that won't work)."
+	bail "It appears you are trying to bookmark an entire issue of a journal. Try navigating to the abstract page for just one article on Ingenta and bookmark that instead (if you're trying to choose which article you want by using the check-boxes on the page then, regrettably, that won't work)."
 }
 
 regexp {ingentaconnect.com[^/]*/(.*)} $url -> path
@@ -50,58 +50,95 @@ set url "http://www.ingentaconnect.com/$path"
 
 set page [url_get $url]
 
+# set page [read [open /tmp/page.txt r]]
+
+
+
 proc get_meta {page name} {
-    set re "<meta name=\"$name\"\[^>\]*content=\"(\[^\"\]*)\"/>"
-    set ret ""
-    regexp $re $page match ret
-    return $ret
+	set re "<meta name=\"$name\"\[^>\]*content=\"(\[^\"\]*)\"/>"
+	set ret ""
+	regexp $re $page match ret
+	return $ret
 }
 
-
+proc get_meta_multi {page name} {
+	set re "<meta name=\"$name\"\[^>\]*content=\"(\[^\"\]*)\"/>"
+	set ret [list]
+	foreach {all item} [regexp -all -inline -- $re $page] {
+		lappend ret $item
+	}
+	return $ret
+}
 
 puts "begin_tsv"
+
 put_tsv title [get_meta $page "DC.title"]
+
+#
+# DOI
+#
 set id [get_meta $page "DC.identifier"]
 if {[regexp {^doi:([^/]+/[^/]+$)} $id -> do]} {
-    put_linkout DOI "" $doi "" ""
+	put_linkout DOI "" $doi "" ""
 }
 
-set rauthors [get_meta $page "DC.creator"]
-foreach author [split $rauthors ";"] {
-    # lose the disporder
-    set author [regsub {\[\d+\]} $author ""]
-    set author [string trim [string map [list "  " " "] $author]]
-    put_tsv author $author
+#
+# Authors live in multiple tags. They used to live in a single tag, spearated by
+# a semicolon, so we'll try to support this as well.
+#
+foreach lauthor [get_meta_multi $page "DC.creator"] {
+	foreach author [split $lauthor ";"] {
+		# lose the disporder
+		set author [regsub {\[\d+\]} $author ""]
+		set author [string trim [string map [list "  " " "] $author]]
+		put_tsv author $author
+	}
 }
 
-
+#
+# Try to extract year/month
+#
 set issued [get_meta $page "DCTERMS.issued"]
-if {[regexp {^(\d+)?\s?(January|February|March|April|May|June|July|August|September|October|November|December) (\d\d\d\d$)} $issued -> ret(day) month ret(year)]} {
-    put_tsv month [map_long_month $month]
-} else {
-    if {[regexp {^(\d\d\d\d)$} $issued -> year]} {
+
+if {[regexp {^(\d+)?\s?(January|February|March|April|May|June|July|August|September|October|November|December) (\d\d\d\d$)} $issued -> ret(day) month year]} {
+	put_tsv month [map_long_month $month]
 	put_tsv year $year
-    }
+} elseif {[regexp {^(\d\d\d\d)$} $issued -> year]} {
+	put_tsv year $year
 }
 
+#
+# Get start/end pages
+#
 set cite [get_meta $page "DCTERMS.bibliographicCitation"]
-if {[regexp {^(.*), (\d+), (\d+), ([0-9-]+)\((\d+)\)$} $cite ->\
-	journal volume issue pages]} {
-    foreach {first last} [parse_start_end_pages $pages] {}
-    if {[info exists first] && $first!=""} {
-	put_tsv start_page $first
-    }
-    if {[info exists last] && $last!=""} {
-	put_tsv end_page $last
-    }
+if {[regexp {^(.*), (\d+), (\d+), ([0-9-]+)\((\d+)\)$} $cite -> journal volume issue pages]} {
+	if {$journal ne ""} {
+		put_tsv journal $journal
+	}
+	foreach {first last} [parse_start_end_pages $pages] {}
+	if {[info exists first] && $first!=""} {
+		put_tsv start_page $first
+	}
+	if {[info exists last] && $last!=""} {
+		put_tsv end_page $last
+	}
 }
+
+#
+# Try for an ISSN
+#
+set part_of [get_meta $page "DCTERMS.isPartOf"]
+if {[regexp {urn:ISSN:(\d\d\d\d-\d\d\d[\dX])$} $part_of -> issn]} {
+	put_tsv issn $issn
+}
+
 
 if {[regexp {<p><strong>Abstract:</strong></p>(.*?)</div>} $page -> abstract]} {
-    put_tsv abstract [string trim [strip_html_tags $abstract]]
+	put_tsv abstract [string trim [strip_html_tags $abstract]]
 }
 
 if {[regexp "value=\"infobike://(\[^\"\]+)\"/>" $page -> infobike]} {
-    put_linkout IBIKE "" $infobike "" ""
+	put_linkout IBIKE "" $infobike "" ""
 }
 
 puts "type\tJOUR"
