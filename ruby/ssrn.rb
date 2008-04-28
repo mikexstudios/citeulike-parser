19274ddbeebbfca1107c521d1b1263b9144e10da
rcameron <rcameron@eb686afc-39fa-0310-a79f-9b3c69cefa1c>
2008-04-28 20:44:36 +0000

    Grrrr. Fix previous commit.
    git-svn-id: http://svn.citeulike.org/svn/plugins@187 eb686afc-39fa-0310-a79f-9b3c69cefa1c

--- ruby/ssrn.rb
+++ ruby/ssrn.rb
@@ -34,7 +34,7 @@ end
 
 puts "journal\tSocial Science Research Network Working Paper Series"
 
-date = (doc/"//tr/td[@width='80%']/center/font[@size='2']")[1]
+date_el = (doc/"//tr/td[@width='80%']/center/font[@size='2']")[1]
 if date_el
   date = date_el.inner_text
   if date =~ /^(January|February|March|April|May|June|July|August|September|October|November|December) ([0-9]{1,2}), ([0-9]{4}$)/
