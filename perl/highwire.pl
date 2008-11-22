f4fd59f7668787bdd092b6001891d8b5edc6d9e8
fgallagh <fgallagh@eb686afc-39fa-0310-a79f-9b3c69cefa1c>
2008-11-22 14:43:43 +0000

    Relaxed the matching criteria for /$volume/$number/$page regex
    git-svn-id: http://svn.citeulike.org/svn/plugins@252 eb686afc-39fa-0310-a79f-9b3c69cefa1c

--- perl/highwire.pl
+++ perl/highwire.pl
@@ -45,6 +45,7 @@ use LWP::UserAgent;
 my $ua = LWP::UserAgent->new;
 
 $url = <>;
+chomp($url);
 
 #Examples of compatible url formats:
 #$url = "http://www.genetics.org/cgi/content/abstract/171/1/35";
@@ -88,7 +89,8 @@ if ($url =~ m{http://([^/]+)/content/((?:[a-zA-Z]+;)?[0-9]+)/([0-9]+)/([A-Za-z0-
 #
 #  Published articles: determine journal,volume,number and page details. 
 #
-elsif ($url =~ m{http://(.*)/cgi(/|/content/)(abstract|short|long|extract|full|refs|reprint|screenpdf|summary|eletters)/((?:[a-zA-Z]+;)?[A-Za-z0-9-.]+)/([0-9]+)/([A-Za-z0-9.]+)}) {
+#elsif ($url =~ m{http://(.*)/cgi(/|/content/)(abstract|short|long|extract|full|refs|reprint|screenpdf|summary|eletters)/((?:[a-zA-Z]+;)?[A-Za-z0-9-.]+)/([0-9]+)/([A-Za-z0-9.]+)}) {
+elsif ($url =~ m{http://(.*)/cgi(/|/content/)(abstract|short|long|extract|full|refs|reprint|screenpdf|summary|eletters)/((?:[a-zA-Z]+;)?[^/]+)/([^/]+)/([^/]+)}) {
 	($journal_site,$volume,$number,$page) = ($1,$4,$5,$6);
 	$url_abstract = "http://$journal_site/cgi/content/refs/$volume/$number/$page";
 } 
