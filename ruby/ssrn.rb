7d7ac4ddb94f1a318e3f587eb1692133b8f4ddd0
fgallagh <fgallagh@eb686afc-39fa-0310-a79f-9b3c69cefa1c>
2009-01-01 23:40:13 +0000

    Added RIS to SSRN
    git-svn-id: http://svn.citeulike.org/svn/plugins@284 eb686afc-39fa-0310-a79f-9b3c69cefa1c

--- ruby/ssrn.rb
+++ ruby/ssrn.rb
@@ -6,12 +6,48 @@ require 'hpricot'
 
 url = gets.strip
 
-doc = Hpricot(open(url, "User-Agent" => "lwp-request/5.810"))
 
-puts "begin_tsv"
+
+doc = Hpricot(open(url, "User-Agent" => "lwp-request/5.810"))
 
 url.match(/abstract_id=(\d+)/)
 id = $1
+
+#<BEGIN_RIS>###########################################################
+#
+# We get the RIS stuff here becuase we need to know if it's available
+# first so we don't duplicate any data, esp. author fields.
+#
+# The RIS/BibTeX data is actually pretty crap - no abstract and the dates are only
+# year.  Since we need to screenscrape that it's arguable whether it's worth doing
+# this at all.
+#
+# bibtex is broken because there's no citation-key and citeulike.bst barfs.
+#
+# TODO: error handling
+
+format = "ris"
+format_id = 3; # 2 for bibtex, 3 for RIS
+
+# A file with a single line like
+# SSRN_LOGIN=me%40domain%2Ecom; SSRN_PW=mypassword
+file = File.new(ENV['HOME']+"/.ssrncookie", "r")
+cookie = file.gets
+file.close
+
+meta = Hpricot(open("http://papers.ssrn.com/sol3/RefExport.cfm?abstract_id=#{id}&format=#{format_id}",
+	"User-Agent" => "lwp-request/5.810",
+	"Cookie" => cookie
+))
+ris = (meta/"//input[@name='hdnContent']")
+#
+# The RIS data is actually dumped at the end - I think it could go here
+# but all the other plugins seem to put it at the end.
+#
+##<END_RIS>#########################################################
+
+
+puts "begin_tsv"
 
 #
 # Look PDF "filename" from a 
@@ -28,6 +64,7 @@ for a in (doc/"//a")
 		break
 	end
 end
+# Why no DOI linkout?
 puts "linkout\tSSRN\t#{id}\t#{filename}\t\t"
 
 puts "type\tJOUR"
@@ -36,8 +73,11 @@ puts "url\thttp://ssrn.com/abstract=#{id}"
 title = (doc/"//div[@id='abstractTitle']").first.inner_text.strip
 puts "title\t" + title
 
-for author in (doc/"//div[@id='innerWhite']/center/font/a[@title='View other papers by this author']")
-  puts "author\t" + author.inner_text.strip.squeeze(" ")
+# Only bother with authors if the RIS wasn't found (otherwise get duplicates)
+if not ris
+  for author in (doc/"//div[@id='innerWhite']/center/font/a[@title='View other papers by this author']")
+    puts "author\t" + author.inner_text.strip.squeeze(" ")
+  end
 end
 
 for input in (doc/"//input[@class='simField']")
@@ -87,7 +127,12 @@ for abs in (doc/"//font[@size='2']")
 	end
 end
 
-
 puts "end_tsv"
+
+if ris
+	puts "begin_#{format}"
+	puts ris.first.attributes['value']
+	puts "end_#{format}"
+end
 
 puts "status\tok"
