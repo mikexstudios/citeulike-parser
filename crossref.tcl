3ee6c4244a34ae11934291edf174b50a0bf4f9e2
fgallagh <fgallagh@eb686afc-39fa-0310-a79f-9b3c69cefa1c>
2009-02-02 13:48:00 +0000

    update crossref for book-chapters
    git-svn-id: http://svn.citeulike.org/svn/plugins@292 eb686afc-39fa-0310-a79f-9b3c69cefa1c

--- crossref.tcl
+++ crossref.tcl
@@ -5,15 +5,13 @@ package require tdom
 namespace eval CROSSREF {
 }
 
-proc CROSSREF::parse_xml {xml {hints {}}} {
-
+#<JOURNALS>#####################################################################
+proc CROSSREF::parse_journal {doc} {
 	array set ret [list]
 
-	set doc [[dom parse $xml] documentElement]
+	set ret(type) JOUR
 
 	set prefix /doi_records/doi_record/crossref/journal
-
-	set ret(type) JOUR
 
 	catch {
 		set ret(journal) [[$doc selectNodes ${prefix}//full_title] text]
@@ -122,4 +120,124 @@ proc CROSSREF::parse_xml {xml {hints {}}} {
 	}
 
 	return [array get ret]
+}
+#<CHAPTERS>#####################################################################
+proc CROSSREF::parse_chapter {doc} {
+	array set ret [list]
+
+	set ret(type) CHAP
+
+	set prefix /doi_records/doi_record/crossref/book
+	set meta "$prefix/book_metadata"
+	set content "$prefix/content_item\[1\]"
+
+	catch {
+		set ret(title_secondary) [[$doc selectNodes ${prefix}/book_metadata/titles/title\[1\]] text]
+	} 
+	catch {
+		set ret(title_series) [[$doc selectNodes ${prefix}/book_metadata/series_metadata/titles/title\[1\]] text]
+	} 
+	
+	# there can be multiple issn e.g., media_type="print"|"electronic"
+	# Which one?  Just choose first one for now.
+	catch {
+		set ret(issn) [[$doc selectNodes ${meta}/issn\[1\]] text]
+	}
+
+	catch {
+		set ret(volume) [[$doc selectNodes ${meta}/volume] text]
+	}
+
+	catch {
+		catch {
+			set ret(day)   [[$doc selectNodes ${meta}/publication_date/day] text]
+		}
+		catch {
+			set ret(month) [[$doc selectNodes ${meta}/publication_date/month] text]
+		}
+		catch {
+			set ret(year)  [[$doc selectNodes ${meta}/publication_date/year] text]
+		}
+	}
+
+	catch {
+		set ret(title) [[$doc selectNodes ${content}/titles/title\[1\]] text]
+		set ret(title) [string trim $ret(title)]
+	}
+
+	#
+	# Authors
+	#    - NB we use the 'sequence' attribute to get author order correct
+	#
+	set a_nodes [$doc selectNodes ${content}/contributors/person_name\[@contributor_role='author'\]]
+
+	set author_list [list]
+
+	foreach a_node $a_nodes {
+		set seq [$a_node getAttribute sequence]
+		if {$seq eq "first"} {
+			set seq A
+		} else {
+			set seq Z
+		}
+		set fname ""
+		set lname ""
+		catch {
+			set fname [[$a_node selectNodes given_name] text]
+		}
+		catch {
+			set lname [[$a_node selectNodes surname] text]
+		}
+
+		lappend author_list [list $seq "$lname, $fname"]
+	}
+
+	foreach a [lsort -ascii -index 0 $author_list] {
+		lappend ret(author) [lindex $a 1]
+	}
+
+	catch {
+		set ret(start_page) [[$doc selectNodes ${content}/pages/first_page] text]
+	}
+	catch {
+		set ret(end_page) [[$doc selectNodes ${content}/pages/last_page] text]
+	}
+
+	catch {
+		set ret(doi) [[$doc selectNodes ${content}/doi_data/doi] text]
+		lappend ret(linkout) [join [list DOI "" $ret(doi) "" ""] \t]
+	}
+	catch {
+		set evpii [[$doc selectNodes ${content}/doi_data/resource] text]
+		if {$evpii ne "" && [regexp {http://linkinghub.elsevier.com/retrieve/pii/(.*)} $evpii -> id]} {
+			set ret(pub_id)      $id
+			set ret(pub_id_type) pii
+			lappend ret(linkout) [join [list EVPII "" $id "" ""] \t]
+		}
+	}
+
+	return [array get ret]
+}
+
+proc CROSSREF::parse_xml {xml {hints {}}} {
+
+	set doc [[dom parse $xml] documentElement]
+	
+	# Journal?
+	catch {
+		if {[$doc selectNodes /doi_records/doi_record/crossref/journal] ne ""} {
+			return [parse_journal $doc]		
+		}
+	}
+	
+	# Chapter?
+	catch {
+		
+		if {[$doc selectNodes /doi_records/doi_record/crossref/book/content_item\[@component_type="chapter"\] ] ne ""} {
+			return [parse_chapter $doc]		
+		}	
+	}
+	
+	return {}
+	
 }
