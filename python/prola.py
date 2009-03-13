b9403e2c31f7909028f8cb16a0c965626eb29357
fgallagh <fgallagh@eb686afc-39fa-0310-a79f-9b3c69cefa1c>
2009-03-13 14:51:49 +0000

    Make PROLA handle all APS journals (it was sooooo close before)
    git-svn-id: http://svn.citeulike.org/svn/plugins@309 eb686afc-39fa-0310-a79f-9b3c69cefa1c

--- python/prola.py
+++ python/prola.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 
-import re, sys, urllib2
+import re, sys, urllib2, cookielib
+from urlparse import urlparse
+from urllib import urlencode
+from urllib2 import urlopen
+
 
 # error messages (taken from iop.py)
 ERR_STR_PREFIX = 'status\terr\t'
@@ -17,17 +21,32 @@ url = sys.stdin.readline()
 # get rid of the newline at the end
 url = url.strip()
 
+cookie_jar = cookielib.CookieJar()
+
+handlers = []
+if "--debug" in sys.argv:
+	handlers.append( urllib2.HTTPHandler(debuglevel=True) )
+handlers.append( urllib2.HTTPCookieProcessor(cookie_jar) )
+
+opener=urllib2.build_opener(*handlers)
+opener.addheaders = [('User-agent', 'lwp-request/5.810')]
+urllib2.install_opener(opener)
+
 # we'll be given a url of the form http://prola.aps.org/abstract/PRA/v18/i3/p787_1
 #  http://prola.aps.org/abstract/journal/volume/issue/page
 # the ris resource is located at http://prola.aps.org/export/___/v__/i__/p__?type=ris
 # use type=bibtex for bibtex 
 # should we prefer one to the other?
 
+parsed_url = urlparse(url)
+
+host = 'http://'+ parsed_url.netloc
+
 address = url.split('abstract')[1]
 
-risurl = 'http://prola.aps.org/export' + address + '?type=ris'
+risurl = host +'/export' + address + '?type=ris'
 try:
-	f = urllib2.urlopen(risurl);
+	f = urlopen(risurl);
 except:
 	print ERR_STR_PREFIX + ERR_STR_FETCH + risurl + '.  ' + ERR_STR_TRY_AGAIN
 	sys.exit(1)
@@ -35,7 +54,7 @@ ris = f.read()
 
 #okay so that handles most of it  but we still need to get the actual title of the journal,
 # and we need to find the abstract if it has one.
-absurl = 'http://prola.aps.org/abstract' + address
+absurl = host +'/abstract' + address
 try:
 	f = urllib2.urlopen(absurl)
 except:
@@ -43,7 +62,7 @@ except:
 	sys.exit(1)
 content = f.read()
 
-match = re.search(r"""aps-abstractbox\s*aps-mediumfont">\s*<p>(.*)</p><br\s/>""", content, re.DOTALL)
+match = re.search(r"""aps-abstractbox\s*aps-mediumtext">\s*<p>(.*)</p><br\s/>""", content, re.DOTALL)
 abstract = ''
 if match:
 	# strip HTML tags (taken from iop.py)
@@ -69,7 +88,22 @@ else:
 	print ERR_STR_PREFIX + ERR_STR_NO_DOI + absurl + '.  ' + ERR_STR_REPORT
 	sys.exit(1)
 
-#We can look at the 3 letter code in the address to get the journal name.
+#We can look at the letter code in the address (/address/xxxxx/) 
+#to get the journal name.
+#Journal  	URL
+#Phys. Rev. A	http://pra.aps.org/ PRA
+#Phys. Rev. B	http://prb.aps.org/ PRB
+#Phys. Rev. C	http://prc.aps.org/ PRC
+#Phys. Rev. D	http://prd.aps.org/ PRD
+#Phys. Rev. E	http://pre.aps.org/ PRE
+#Phys. Rev. Lett.	http://prl.aps.org/ PRL
+#Phys. Rev. ST Phys. Educ. Res.	http://prst-per.aps.org/ PRSTPER
+#Phys. Rev. ST Accel. Beams	http://prst-ab.aps.org/ PRSTAB
+#Rev. Mod. Phys.	http://rmp.aps.org/ RMP
+#PROLA Archive	http://prola.aps.org/ PR
+# ??? PRI
+
+journal_key = address.split("/")[1]
 	
 journalmap = {'PRA' : 'Physical Review A',
               'PRB' : 'Physical Review B',
@@ -78,10 +112,16 @@ journalmap = {'PRA' : 'Physical Review A',
               'PRE' : 'Physical Review E',
               'PRL' : 'Physical Review Letters',
               'PRI' : 'Physical Review (Series I)',
-              'PR/'  : 'Physical Review',
-              'RMP' : 'Reviews of Modern Physics'
+              'PR' : 'Physical Review Online Archive (Prola)',
+              'RMP' : 'Reviews of Modern Physics',
+              'PRSTAB' : 'Physical Review Special Topics - Accelerators and Beams',
+              'PRSTPER' : 'Phys. Rev. ST Phys. Educ. Res.'
              }
-journal = journalmap[address[1:4]]
+journal = journalmap[journal_key]
+#try:
+#	journal = journalmap[address[1:4]]
+#except KeyError:
+#	journal = "APS Journal"
  
 print 'begin_tsv'
 print 'journal\t' + journal
