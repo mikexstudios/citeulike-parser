e3f99518dab8a7ee6c0aea4da3e47c034909e5d6
fgallagh <fgallagh@eb686afc-39fa-0310-a79f-9b3c69cefa1c>
2009-06-17 10:53:59 +0000

    Improved crossref code
    git-svn-id: http://svn.citeulike.org/svn/plugins@334 eb686afc-39fa-0310-a79f-9b3c69cefa1c

--- perl/crossref-doi.pl
+++ perl/crossref-doi.pl
@@ -60,41 +60,34 @@ $in_url =~ m{^http://dx\.doi\.org/(.*)}i;
 $doi = $1;
 
 # pid=username:password (private!)
-# (we already have a similar file .crossref-key so should use that)
 my $keydata=`cat $ENV{HOME}/.crossref-key`;
 my ($pid) = $keydata =~ m{(\w+:\w+)};
 
 $url="http://www.crossref.org/openurl/?id=doi:$doi&noredirect=true&pid=$pid&format=unixref";
 
-# print "URL=$url\n";
-
 $response = $browser->get("$url") or (print "status\terr\t Could not retrieve information from the specified page. Try posting the article from the abstract page.\n" and exit);
 $body = $response->content;
 
-# print $body;
-
 #
 # Bizarrely, there's no XML decl ("<?xml..") so the encoding's assumed somewhere
-# (not here).  Crossref can write a complicated XSD schema but not simple XML :-)
+# (not here).
 #
 $xml = new XML::Simple;
-# read XML file
-#$data = $xml->XMLin("crossref-doi-sample.xml", ForceArray => [qw/title titles person_name identifier/]);
+# read XML file (not really necessary as crossref.tcl has to do this anyway)
 $data = $xml->XMLin($body, ForceArray => [qw/title titles person_name identifier issn/]);
+
+$base = $data->{"doi_record"}->{"crossref"};
+
+if (!$base) {
+	print "status\terr\t  That DOI does not appear to be a known type.\n";
+	exit;
+}
 
 $error = $data->{"doi_record"}->{"crossref"}->{"error"};
 if ($error) {
 	print "status\terr\t  Could not retrieve the information for that DOI. Crossref returned: $error\n";
 	exit;
 }
-
-$base = $data->{"doi_record"}->{"crossref"}->{"journal"};
-
-if (!$base) {
-	print "status\terr\t  That DOI does not appear to be a journal.\n";
-	exit;
-}
-
 
 print "begin_tsv\n";
 print "type\tJOUR\n";
@@ -104,72 +97,3 @@ print "begin_crossref\n";
 print "$body\n";
 print "end_crossref\n";
 print "status\tok\n";
-
-
-#########################
-#nothing below here used - historic (as of 2008-11-26)
-#
-sub value {
-	($ref) = @_;
-	if (ref($ref) eq 'ARRAY') {
-		return $ref->[0];
-	}
-	return $ref;
-}
-
-sub out {
-	local ($key, $value) = @_;
-	print "$key\t$value\n" if $value;
-}
-
-sub xxxx {
-$article = $base->{"journal_article"};
-$journal_metadata=$base->{"journal_metadata"};
-$journal_issue=$base->{"journal_issue"};
-
-# print Dumper($base);
-print "begin_tsv\n";
-out("journal",$journal_metadata->{"full_title"});
-out("issn",$journal_metadata->{"issn"}->[0]->{"content"});
-out("issn",$journal_metadata->{"issn"}->[1]->{"content"});
-
-# Only take first of each of titles/title.
-# TODO: maybe choose one with lang=en?
-$titles=$article->{titles}->[0];
-out("title", $titles->{title}->[0]);
-
-out("volume", 		$journal_issue->{"journal_volume"}->{"volume"});
-out("issue", 		$journal_issue->{"issue"});
-out("day", 			$article->{"publication_date"}->{"day"});
-out("month", 		$article->{"publication_date"}->{"month"});
-out("year", 		$article->{"publication_date"}->{"year"});
-out("start_page", 	$article->{"pages"}->{"first_page"});
-out("end_page", 	$article->{"pages"}->{"last_page"});
-out("doi",			$article->{"doi_data"}->{"doi"});
-out("url",			$in_url);
-
-$persons = $article->{"contributors"}->{"person_name"};
-# sequence='first'|'additional', so sort on this key in reverse alphabetical order
-# (I suspect they're always in the right order already)
-for $p (sort { $b->{sequence} cmp $a->{sequence}} @$persons) {
-	if ($p->{"contributor_role"} eq "author") {
-		print "author\t".$p->{"surname"}.", ".$p->{"given_name"}."\n";
-	}
-}
-
-# publisher
-# TODO check this - nothing in HOWTO.txt
-$publisher = $article->{"publisher_item"}->{"identifier"}->[0];
-if ($publisher) {
-	out("pub_id", $publisher->{"content"});
-	out("pub_id_type", $publisher->{"id_type"});
-	print "linkout\tEVPII\t\t".$publisher->{"content"}."\t\t\n";
-}
-
-$DOI = $article->{"doi_data"}->{"doi"};
-print "linkout\tDOI\t\t$DOI\t\t\n" if $DOI;
-print "type\tJOUR\n";
-
-print "end_tsv\n";
-
-}
