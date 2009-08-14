5a70698f0167b95fb1f42929d10aea46ea8243bf
fgallagh <fgallagh@eb686afc-39fa-0310-a79f-9b3c69cefa1c>
2009-08-14 15:30:59 +0000

    BeautifulSoup seems to do this better
    git-svn-id: http://svn.citeulike.org/svn/plugins@418 eb686afc-39fa-0310-a79f-9b3c69cefa1c

--- python/utils.py
+++ python/utils.py
@@ -3,7 +3,12 @@
 import re
 from htmlentitydefs import name2codepoint
 
+from BeautifulSoup import BeautifulStoneSoup
+
 def decode_entities(html):
+	return unicode(BeautifulStoneSoup(html,convertEntities=BeautifulStoneSoup.HTML_ENTITIES ))
+
+def x_decode_entities(html):
 	html = re.sub('&#(\d+);', lambda m: unichr(int(m.group(1))), html)
 	html = re.sub('&(%s);' % '|'.join(name2codepoint), lambda m: str(name2codepoint[m.group(1)]), html)
 	return html
