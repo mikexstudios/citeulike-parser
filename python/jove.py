e78db431a24724f54fe5bda9d0afab1fd703550b
fgallagh <fgallagh@eb686afc-39fa-0310-a79f-9b3c69cefa1c>
2011-02-24 23:43:45 +0000

    Updated JOVE
    git-svn-id: http://svn.citeulike.org/svn/plugins@636 eb686afc-39fa-0310-a79f-9b3c69cefa1c

--- python/jove.py
+++ python/jove.py
@@ -55,64 +55,23 @@ def item(soup, entry, key):
 
 def handle(url):
 
-	m = re.match(r'http://www\.jove.com/index/Details\.stp\?ID=(\d+)', url)
+	m = re.match(r'http://www\.jove.com/(?:index/Details\.stp|Details.php|details.stp)\?ID=(\d+)', url)
 	if not m:
 		raise ParseException, "URL not supported %s" % url
 	wkey = m.group(1)
 
-	page = urlopen(url).read()
+	ris_url = "http://www.jove.com/Resources/php/GetRIS.php?id=%s" % wkey
 
-	soup = BeautifulSoup.BeautifulSoup(page)
+	ris = urlopen(ris_url).read()
 
-	head = soup.find("head")
-
-	doi = meta(head, 'citation_doi')
-
-	if not doi:
-		raise ParseException, "Cannot find DOI"
 
 	print "begin_tsv"
-	print "linkout\tDOI\t\t%s\t\t" % (doi)
 	print "linkout\tJOVE\t%s\t\t\t" % wkey
 	print "type\tJOUR"
-	title = meta(head, "citation_title")
-	if title:
-		print "title\t%s" % unescape(title)
-	item(head, "journal", "citation_journal_title")
-	item(head, "issue", "citation_issue")
-	date = meta(head, 'citation_date')
-	if date:
-		m = re.match(r'(\d+)/(\d+)/(\d+)', date)
-		if m:
-			# ARGH: American m/d/y
-			day = m.group(2)
-			month = m.group(1)
-			year = m.group(3)
-			if year:
-				print "year\t%s" % year
-			if month:
-				print "month\t%s" % month
-			if day:
-				print "day\t%s" % day
-
-	# authors
-	authors = head.findAll("meta", {"name":"dc.Contributor"})
-	if authors:
-		for a in authors:
-			print "author\t%s" % a['content']
-
-	#
-	# This seems very fragile.
-	#
-	abstract = soup.find('div', {'id':'C47_c1vzp1s1'})
-	if abstract:
-		abs = abstract.renderContents().strip()
-		abs = re.sub(r'<[^>]+>','',abs)
-		abs = unescape(abs)
-		print "abstract\t%s" % abs
-
-	print "doi\t%s" % doi
 	print "end_tsv"
+	print "begin_ris"
+	print ris
+	print "end_ris"
 	print "status\tok"
 
 # read url from std input
